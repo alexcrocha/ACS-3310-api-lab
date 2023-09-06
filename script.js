ce02beb61c1d80f6670dddac16acc8bf445d2c9c
Alex Rocha <rocha.amc@gmail.com>
2023-09-06 10:14:27 -0700

    Move getWeather() to its own file

---
+++ script.js
@@ -0,0 +1,13 @@
+// Functions
+export function getWeather(zip) {
+  // Replace this with your own API key!
+  const units = 'imperial'
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+  fetch(path)
+    .then(res => res.json())
+    .then(json => {
+      tempEl.innerHTML = json.main.temp
+      descEl.innerHTML = json.weather[0].description
+    })
+    .catch(err => console.log(err.message))
+}
