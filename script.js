1608b95c6965a13027b6671e6f84a1011c0b9189
Alex Rocha <rocha.amc@gmail.com>
2023-09-06 11:54:32 -0700

    Parse json

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ export async function getWeather(apiKey, zip, updateUI) {
   try {
     const res = await fetch(path);
     const json = await res.json();
-    updateUI(json);
+    const data = {
+      temp: json.main.temp,
+      description: json.weather[0].description,
+    }
+    updateUI(data);
   } catch (err) {
     console.log(err.message);
   }
