f1aec1bfa175c6a674ebe1fadaca480924a12814
Alex Rocha <rocha.amc@gmail.com>
2023-09-06 11:39:32 -0700

    Refactor with async/await

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-// Functions
-export function getWeather(apiKey, zip, updateUI) {
-  // Replace this with your own API key!
-  const units = 'imperial'
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  fetch(path)
-    .then(res => res.json())
-    .then(json => updateUI(json))
-    .catch(err => console.log(err.message))
+export async function getWeather(apiKey, zip, updateUI) {
+  const units = 'imperial';
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+  try {
+    const res = await fetch(path);
+    const json = await res.json();
+    updateUI(json);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
