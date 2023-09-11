fcdcb363cf5156bad24583725f2d672d866ba48c
Alex Rocha <rocha.amc@gmail.com>
2023-09-10 22:28:32 -0700

    Allow user to input zip code, city name, or geo location

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-export async function getWeather(apiKey, zip, units, updateUI) {
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+const parseInput = (input) => {
+  const zip = /^\d{5}$/.test(input) ? input : null;
+  const city = /^[a-zA-Z\s]+$/.test(input) ? input : null;
+  const geo = /^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/.test(input) ? input.split(',') : null;
+  return { zip, city, geo };
+};
+
+export async function getWeather(apiKey, input, units, updateUI) {
+  const { zip, city, geo } = parseInput(input);
+  const query = zip ? `zip=${zip}` : geo ? `lat=${geo[0].trim()}&lon=${geo[1].trim()}` : `q=${city}`;
+  const path = `https://api.openweathermap.org/data/2.5/weather?${query}&appid=${apiKey}&units=${units}`;
+
   try {
     const res = await fetch(path);
     const json = await res.json();
@@ -10,5 +20,7 @@ export async function getWeather(apiKey, zip, units, updateUI) {
     updateUI(data);
   } catch (err) {
     console.log(err.message);
+    updateUI({ temp: '', description: 'Location not found' });
   }
 }
+
